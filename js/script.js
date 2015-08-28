2cb7051f4c59205ad0634b80af47f79e319c3f55
Zac Clemans <zaclem01@gmail.com>
2015-08-27 23:34:39 -0400

    new grid creation method

--- js/script.js
+++ js/script.js
@@ -1,21 +1,48 @@
-/*$(document).ready(function() {
-	for(var row = 0; row < 16; row++) {
-		$('.container').append('<div class="row' + row + '"></div>')
-		for(var column = 0; column < 16; column++) {
-			$('.container').find('.row' + row).append('<div class="grid">t</div>');
-		}
-	}
-});*/
+var initialSize = 64;
 
 $(document).ready(function() {
+	createGrid(initialSize);
+	blackHighlight();
+});
+
+function createGrid(size) {
+
+	gridWidth = 960 / size;
+
+	for(var row = 0; row < size; row++) {
+		for(var column = 0; column < size; column++) {
+			$('.container').append('<div class="square"></div>');
+		}
+		$('.container').append('<div class="row"></div>');
+	}
+
+	$('.square').css({'width':gridWidth, 'height':gridWidth});
+}
+
+function blackHighlight() {
+	$('.square').on('mouseenter', function() {
+		$(this).addClass('highlight');
+	});
+}
+
+/* Tried this method but it was much too slow*/
+
+/*$(document).ready(function() {
 	$('.container').append("<table></table>");
+
+	var tableHolder = "";
+
 	for(row = 0; row < 16; row++) {
 		var tableRow = "<tr>";
 		for(column = 0; column < 16; column++) {
-			tableRow = tableRow.concat("<td>t</td>");
+			tableRow = tableRow.concat("<td></td>");
 		}
 		tableRow = tableRow.concat("</tr>");
-		var toAppend = $(tableRow);
-		$('.container').find('table').append(toAppend);
+		tableHolder = tableHolder.concat(tableRow);
 	}
-});
+
+	$('.container').find('table').append(tableHolder);
+
+	blackHighlight();
+
+});*/
